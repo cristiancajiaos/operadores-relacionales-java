e4eac069dc50abb357e0e860970675af118a8119
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 12:07:33 -0400

    Agregados operadores relacionales

--- src/operadores/relacionales/OperadoresRelacionales.java
+++ src/operadores/relacionales/OperadoresRelacionales.java
@@ -4,7 +4,15 @@ package operadores.relacionales;
 public class OperadoresRelacionales {
 
     public static void main(String[] args) {
-        
+        int a = 10, b = 5;
+    
+        // Operadores relacionales
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a != b: " + (a != b));
     }
     
 }
